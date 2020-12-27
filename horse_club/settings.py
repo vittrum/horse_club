ecc4b1cb42a1af4624595bfeee4da462e6f6e37f
vittrum <viktorkovalskyi9@gmail.com>
2020-12-28 01:43:51 +0200

    front prepare

--- horse_club/settings.py
+++ horse_club/settings.py
@@ -48,8 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'psycopg2',
     'django',
-    'django_filters',
-
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'user.User'
@@ -104,6 +103,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'horse_club.urls'
@@ -175,3 +175,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
